ef8caab86a814954399fe66bef3807418ece0053
fg4n <fgan1@hotmail.com>
2019-04-02 11:46:36 -0300

    Adding fonts

--- src/js/sunburst-constants.js
+++ src/js/sunburst-constants.js
@@ -1,5 +1,5 @@
 const GEOSERVER_URL = 'http://150.165.85.9/geoserver/ows/'
-const GEONODE_URL = 'http://150.165.85.24'
+const GEONODE_URL = 'http://150.165.85.9'
 
 const DEFAULT_LAYER_OPACITY = 0.85;
 const CENTER_COORDS = [-4490828.28581067, -1124945.094166];
