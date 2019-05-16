7bba175f20080510e88d83633072eb0b6952feaa
users2010 <users2010@gmail.com>
2019-05-16 10:33:16 -0300

    Update sunburst-constants.js

--- src/js/sunburst-constants.js
+++ src/js/sunburst-constants.js
@@ -1,5 +1,5 @@
-const GEOSERVER_URL = 'http://192.168.15.13/geoserver/ows'
-const GEONODE_URL = 'http://192.168.15.13'
+const GEOSERVER_URL = 'http://192.168.2.107/geoserver/ows'
+const GEONODE_URL = 'http://192.168.2.107'
 
 const DEFAULT_LAYER_OPACITY = 0.85;
 const CENTER_COORDS = [-4490828.28581067, -1124945.094166];
